b0a80393c080b74abd372e790d8e586f539d8884
krafay92 <rafaywork93@gmail.com>
2021-01-23 11:31:06 +0500

    Assignment 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Comp from './Comp.js';
 
-function App() {
-  return (
-    <div className="App">
-      <Comp type="function component" location="App.js" />
+
+function App() {  
+  let [count,increser] = useState(0);
+  let [night,changer] = useState(true);
+
+  return(
+    <div className = {`${ night ? 'night' : 'mor'}`}>
+      <h1>Good { night ? 'Night' : 'Morning'}</h1>
+      < Comp counter = {count}/>
+      <button onClick = { () => increser(++count)}>Increse</button>
+      <button onClick = { () => changer(!night)}>Change day</button>
     </div>
   );
-}
 
+}
 export default App;
--- src/Comp.js
+++ src/Comp.js
@@ -2,10 +2,7 @@ import React from 'react';
 
 function Comp(props){
     return(
-        <div>
-            <h2>This is an example of {props.type}</h2>
-            <h2>It is inside of {props.location}</h2>
-        </div>
+       <h1>Counter Value is : {props.counter}</h1>
     );
 }
 
